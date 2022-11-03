c169b104eb8a481acf051864b34ef8d6e026c965
ekorahy <ekorahy@gmail.com>
2022-11-03 23:03:27 +0700

    change margin top for hero section

--- components/Hero.js
+++ components/Hero.js
@@ -20,14 +20,14 @@ function Hero() {
           <h2 className="font-semibold text-primary text-lg font-Kanit md:text-2xl lg:text-3xl">{`Hi, I'm Eko Rahayu Widodo`}</h2>
           <h1 className="text-white font-Kanit font-bold text-3xl md:text-4xl lg:text-5xl lg:mt-1">Front-End Web Developer.</h1>
           <p className="text-body font-Roboto text-sm mt-3 leading-relaxed lg:text-base lg:mt-4 lg:w-4/5">{`from Jakarta, Indonesia. Someone who is highly motivated to continue to grow, likes challenges and able to work in a team or individually. I always give the best effort for each projects I did and I give a solution with my creative app.`}</p>
-          <div className="flex items-center mt-2 lg:mt-4">
+          <div className="flex items-center mt-3 lg:mt-4">
             <ButtonBgPrimary to="/" icon={<TfiEmail />} description="Email Me" />
             <ButtonIconSocial to="/" icon={<AiFillLinkedin />} color="text-linkedin" title="button to Linkedin" />
             <ButtonIconSocial to="/" icon={<AiFillGithub />} color="text-github" title="button to Github" />
             <ButtonIconSocial to="/" icon={<AiOutlineInstagram />} color="text-instagram" title="button to Instagram" />
             <ButtonIconSocial to="/" icon={<AiOutlineWhatsApp />} color="text-whatsapp" title="button to WhatsApp" />
           </div>
-          <div className="mt-2 md:ml-1 lg:mt-4 lg:ml-2">
+          <div className="mt-3 lg:mt-4 lg:ml-2">
             <Link href="/" className="flex items-center text-sm font-bold text-primary hover:text-fuchsia-600"><HiDownload className="text-lg mr-2" /> Download CV</Link>
           </div>
         </div>
